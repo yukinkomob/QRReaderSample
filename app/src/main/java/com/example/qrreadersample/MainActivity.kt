27df2ed699532479c27f7d45699886869f442b98
Yuki Nishimura <yukinkopen@gmail.com>
2022-01-31 17:57:09 +0900

    バーコード表示機能とカメラ起動機能を追加

--- app/src/main/java/com/example/qrreadersample/MainActivity.kt
+++ app/src/main/java/com/example/qrreadersample/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.qrreadersample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).apply {
+            setOnClickListener {
+                onButtonClick(it)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        makeBarcode()
+    }
+
+    // Register the launcher and result handler
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun launchBarcodeReader() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES)
+        options.setPrompt("Scan a barcode")
+        options.setCameraId(0) // Use a specific camera of the device
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        barcodeLauncher.launch(options)
+    }
+
+    private fun makeBarcode() {
+        try {
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.encodeBitmap("content", BarcodeFormat.QR_CODE, 400, 400)
+            val imageViewQrCode = findViewById<View>(R.id.qr_code) as ImageView
+            imageViewQrCode.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+        }
+    }
+
+    // Launch
+    private fun onButtonClick(view: View) {
+        launchBarcodeReader()
     }
 }
